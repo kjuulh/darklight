eab6f84e2e4081f445831d810d22f22b6436efc8
kjuulh <contact@kjuulh.io>
2022-05-04 22:51:57 +0200

    Style page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,108 +1,126 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { FC, useEffect, useState } from "react";
+import type {NextPage} from "next";
+import {FC, useEffect, useState} from "react";
 import axios from "axios";
 
+import styles from '../styles/index.module.scss'
+
 type GetDownloadResponse = {
-  id: string;
-  link: string;
-  state: string;
-  file_name?: string;
+    id: string;
+    link: string;
+    state: string;
+    file_name?: string;
 };
 
 interface DownloadingFileProps {
-  id: string;
+    id: string;
 }
 
 const DownloadingFile: FC<DownloadingFileProps> = (props) => {
-  const [fileUrl, setFileUrl] = useState<GetDownloadResponse | undefined>();
+    const [fileUrl, setFileUrl] = useState<GetDownloadResponse | undefined>();
 
-  useEffect(() => {
-    axios
-      .get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/download/${props.id}`)
-      .then((res) => {
-        setFileUrl(res.data);
-      })
-      .catch(console.error);
-  }, [props.id]);
+    useEffect(() => {
+        axios
+            .get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/download/${props.id}`)
+            .then((res) => {
+                setFileUrl(res.data);
+            })
+            .catch(console.error);
+    }, [props.id]);
 
-  if (!fileUrl) {
-    return <div>Loading...</div>;
-  }
+    if (!fileUrl) {
+        return <div>Loading...</div>;
+    }
 
-  return (
-    <div>
-      <a
-        href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/download/${props.id}/file`}
-        download={fileUrl.file_name}
-      >
-        {fileUrl.file_name}
-      </a>
-    </div>
-  );
+    return (
+        <div>
+            <a
+                href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/download/${props.id}/file`}
+                download={fileUrl.file_name}
+            >
+                {fileUrl.file_name}
+            </a>
+        </div>
+    );
 };
 
 const Home: NextPage = () => {
-  const [downloadingFiles, setDownloadingFiles] = useState<string[]>([]);
-  const [url, setUrl] = useState("");
+    const [downloadingFiles, setDownloadingFiles] = useState<string[]>([]);
+    const [url, setUrl] = useState("");
 
-  const initiateDownload = (downloadLink: string) => {
-    axios
-      .post(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/download`,
-        {
-          link: downloadLink,
-        },
-        {}
-      )
-      .then((res) => {
-        setDownloadingFiles((files) => [...files, res.data]);
-      })
-      .catch(console.error);
-  };
+    const initiateDownload = (downloadLink: string) => {
+        axios
+            .post(
+                `${process.env.NEXT_PUBLIC_BACKEND_URL}/download`,
+                {
+                    link: downloadLink,
+                },
+                {}
+            )
+            .then((res) => {
+                setDownloadingFiles((files) => [...files, res.data]);
+            })
+            .catch(console.error);
+    };
 
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Download file</h1>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
+    return (
+        <div className={styles.container}>
+            <nav className={styles.nav}>
+                <div className={styles.nav_container}>
+                    <div className={styles.icon}>
+                    </div>
+                    <p>
+                        Darklight
+                    </p>
+                </div>
+            </nav>
+            <main className={styles.main}>
+                <h1 className={styles.title}>Download file</h1>
 
-            if (url !== "") {
-              initiateDownload(url);
-            }
-          }}
-        >
-          <input
-            name="url"
-            type="text"
-            placeholder="File to download"
-            required
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-          />
-          <button type="submit">Download</button>
-        </form>
+                <form
+                    className={styles.form}
+                    onSubmit={(e) => {
+                        e.preventDefault();
 
-        <section>
-          <h2>Downloaded files</h2>
+                        if (url !== "") {
+                            initiateDownload(url);
+                        }
+                    }}
+                >
+                    <div className={styles.input_group}>
+                        <input
+                            className={styles.download_input}
+                            name="url"
+                            type="text"
+                            placeholder="File to download"
+                            required
+                            value={url}
+                            autoComplete="off"
+                            onChange={(e) => setUrl(e.target.value)}
+                        />
+                        <button type="submit">Download</button>
+                    </div>
+                </form>
 
-          <div>
-            <ul>
-              {downloadingFiles.map((df, i) => (
-                <li key={i}>
-                  <DownloadingFile id={df} />
-                </li>
-              ))}
-            </ul>
-          </div>
-        </section>
-      </main>
-    </div>
-  );
+                {
+                    downloadingFiles.length > 0 && (
+                        <section>
+                            <h2 className={styles.sub_title}>Downloaded files</h2>
+
+                            <div>
+                                <ul>
+                                    {downloadingFiles.map((df, i) => (
+                                        <li key={i}>
+                                            <DownloadingFile id={df}/>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </section>
+                    )
+                }
+            </main>
+        </div>
+    );
 };
 
 export default Home;
